e85d323b63c097d81fdba85d186665269785db9d
ValentinMorel <valntn.mrl@gmail.com>
2020-08-22 12:58:33 +0200

    Implement the structure of a single task for mongodb.

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module todo-app
 go 1.14
 
 require (
+	github.com/davecgh/go-spew v1.1.1
 	github.com/gorilla/mux v1.7.4 // indirect
-	go.mongodb.org/mongo-driver v1.4.0 // indirect
+	go.mongodb.org/mongo-driver v1.4.0
 )
--- server/main.go
+++ server/main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"todo-app/server/models"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+
+	"github.com/davecgh/go-spew/spew"
+)
+
+func main() {
+	id := primitive.NewObjectID()
+	tasklist := models.TaskList{1, id, "test", true}
+	spew.Dump(tasklist)
+}
