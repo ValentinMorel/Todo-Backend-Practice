5d0910c00cffd1d09146d4f1a8e158013a761e0d
ValentinMorel <valntn.mrl@gmail.com>
2020-08-23 23:02:28 +0200

    Implement insert and delete functions for MongoDB.

--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.14
 
 require (
 	github.com/davecgh/go-spew v1.1.1
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/gommon v0.3.0 // indirect
 	go.mongodb.org/mongo-driver v1.4.0
 )
--- server/middleware/middleware.go
+++ server/middleware/middleware.go
@@ -1,11 +1,16 @@
-package middleware
+package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net/http"
+	"todo-app/server/models"
 
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 
 	"go.mongodb.org/mongo-driver/mongo"
@@ -43,4 +48,64 @@ func init() {
 
 	collection = client.Database(dbname).Collection(collname)
 	fmt.Println(collection)
+}
+
+// Get task route
+func GetAllTask(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	payload := getalltask()
+	json.NewEncoder(w).Encode(payload)
+}
+
+// Get all tasks from DB
+func getalltask() []primitive.M {
+	cur, err := collection.Find(context.Background(), bson.D{{}})
+	if err != nil {
+		log.Fatal(err)
+	}
+	var results []primitive.M
+	for cur.Next(context.Background()) {
+		var result bson.M
+		e := cur.Decode(&result)
+		if e != nil {
+			log.Fatal(e)
+		}
+		results = append(results, result)
+	}
+	if err := cur.Err(); err != nil {
+		log.Fatal(err)
+	}
+	cur.Close(context.Background())
+	return results
+}
+
+func insertonetask(task models.TaskList) {
+	insertresult, err := collection.InsertOne(context.Background(), task)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Inserted a record : ", insertresult.InsertedID)
+}
+
+func deleteonetask(task string) {
+	id, _ := primitive.ObjectIDFromHex(task)
+	filter := bson.M{"_id": id}
+	d, err := collection.DeleteOne(context.Background(), filter)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Deleted Document : ", d.DeletedCount)
+}
+
+func main() {
+	//id := primitive.NewObjectID()
+
+	//tasklist := models.TaskList{1, id, "test", true}
+	//insertonetask(tasklist)
+	//results := getalltask()
+	//fmt.Println(results)
+	deleteonetask("5f42934eaf2fb05a5599702a")
+
 }
